c903e74c9ea1641a7699fd1375a163b4557cbd78
Blackout <jsthompsonuk07@gmail.com>
2022-06-15 20:27:48 +0100

    Bg of home page, original taskbar in progress

--- components/layout.js
+++ components/layout.js
@@ -1,5 +1,7 @@
 import Navbar from "./Navbar"
 import Footer from "./footer"
+import Triangulr from "triangulr"
+import React, {useRef, useEffect} from "react"
 export default function Layout({children}) {
         return (
                 <div>
